d849242682410acb0240aa318ea2407f815cff64
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-04-01 11:00:25 +0200

    added comments and basic buttons

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -1,7 +1,16 @@
+/**
+ * Pending tasks:
+ * 1- Recieve user's name through props
+ * 2- Insert user's name in h1
+ * 
+ */
 const Profile = () => {
     return(
         <div>
-            
+            <h1>Hello {}!</h1>
+            <button>See Pets</button>
+            <button>See Appointments</button>
+            <button>Logout</button>
         </div>
     )
 }
