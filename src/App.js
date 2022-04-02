dd4397e8d967e3ab87d96ef09c0c80fc91a8ff7c
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-04-02 17:01:26 +0200

    added some routes to test react-router-dom

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import Register from './containers/Register/Register.js';
 import RegisterPet from './containers/RegisterPet/RegisterPet.js';
 import Login from './containers/Login/Login.js';
 import Home from './containers/Home/Home.js';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Profile from './containers/Profile/Profile';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/register/pet' element={<RegisterPet />} />
 
-    <Register></Register>
-
-    
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
