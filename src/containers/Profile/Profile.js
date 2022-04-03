a2920726602d58ac9078589fccbb11ab82cf0e53
SusanaChen <grupochen@hotmail.com>
2022-04-03 12:07:24 +0200

    profile´s function ok

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+
+
 /**
  * Pending tasks:
  * 1- Recieve user's name through props
@@ -5,12 +8,40 @@
  * 
  */
 const Profile = () => {
+
+    const [pet, setPet] = useState([]);
+
+    const petList = async =()=>{
+
+        const petResults = await fetch("https://chen-veterinary.herokuapp.com/pet", {
+            method:"GET"
+        });
+
+        const dataPet = await petResults.json();
+
+        setPet(dataPet);
+    }
+
+    useEffect(()=>{
+        try{
+            petList();
+        } catch (error){
+            console.log(error)
+        }
+    },[]);
+
     return(
         <div>
             <h1>Hello {}!</h1>
             <button>See Pets</button>
             <button>See Appointments</button>
             <button>Logout</button>
+            <button>Add new pet</button>
+            <div>
+                <ol>
+
+                </ol>
+            </div>
             
 
         </div>
