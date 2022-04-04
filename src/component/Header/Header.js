bd6c71f46aa2fb2dbb57ce9523d1581f8824d19f
SusanaChen <grupochen@hotmail.com>
2022-04-04 13:33:50 +0200

    new header

---
+++ src/component/Header/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return(
+        <div>
+            <a href="../../containers/Home/Home.js"></a>
+            <a href="../../containers/Profile/Profile.js"></a>
+            <a href="../../containers/ListAppointments/ListAppointment.js"></a>
+            <button>LogiN</button>
+        </div>
+    )
+}
